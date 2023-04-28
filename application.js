953af69c75160b54baf795e0ad6c71718cfb2db1
CAMBAM30 <cmcdondle@gmail.com>
2023-04-28 01:10:46 -0600

    js add remove, add & cart item total

--- application.js
+++ application.js
@@ -4,6 +4,30 @@ $(document).ready(function () {
    var Quantity = parseFloat($(ele).children(".qty").text());
 
       var itemTotal = price * Quantity;
-      console.log(itemTotal);
+      $(ele).children(".itemTotal").html(itemTotal);
+
+      return itemTotal;
+   });
+   $(document).on("click", ".btn.remove", function (event) {
+      $(this).closest("tr").remove();
+   });
+
+   $(document).on("input", "tr input", function () {
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+         updateTotalPrice();
+      }, 1000);
+   });
+
+   $("#addItem").on("submit", function (event) {
+      event.preventDefault();
+      var name = $(this).children("[name=name]").val();
+      var price = $(this).children("[name=price]").val();
+      var qty = $(this).children("[name=qty]").val();
+      var itemTotal = $(this).children("[name=itemTotal]").val();
+
+      $("tbody").append('<tr>' +' <td class="name">' + name + "</td>" +
+      '<td class="price">' + price + "</td>" + '<td class="qty">' + qty + "</td>" +
+      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' + '<td class="itemTotal">' + itemTotal + "</td>");
    });
 });
