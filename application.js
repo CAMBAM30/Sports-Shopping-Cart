064dae873cdef186ce28b10ad3e9f29588dc03b2
CAMBAM30 <cmcdondle@gmail.com>
2023-04-30 00:54:25 -0600

    new edits qty & add total

--- application.js
+++ application.js
@@ -1,42 +1,43 @@
 $(document).ready(function () {
-  $("tbody tr").each(function (i,ele) {
-   var price = parseFloat($(ele).children('.price').text());
-   var Quantity = parseFloat($(ele).children('.qty').text());
+   var updateTotalPrice = function () {
+      $("tbody tr").each(function (i,ele) {
+         var price = parseFloat($(ele).find('.price').text());
+         var Quantity = parseFloat($(ele).find('.qty input').val());
+         console.log(ele)
+         var itemTotal = price * Quantity;
+         console.log(itemTotal)
+         $(ele).find(".itemTotal").html(itemTotal);
 
-      var itemTotal = price * Quantity;
-      $(ele).children(".itemTotal").html(itemTotal);
-
-      return itemTotal;
-   });
-   var priceTotal = 0;
-   $('td[class*=".itemTotal"]').each(function () {
-      priceTotal += parseFloat($(this).children(".itemTotal").text());
-   });
-   $('#priceTotal').text(priceTotal);
-
-  
+         return itemTotal;
+      });
+      var priceTotal = 0;
+      $('td[class*=".itemTotal"]').each(function () {
+         priceTotal += parseFloat($(this).children(".itemTotal").text());
+      });
+      $('#priceTotal').text(priceTotal);
+      }
+      updateTotalPrice();
 
    $(document).on("click", ".btn.remove", function (event) {
       $(this).closest("tr").remove();
    });
-
-   $(document).on("input", "tr input", function () {
-      clearTimeout(timeout);
+   var timeout = undefined;
+   $(document).on("keyup", ".qty input", function() {
+      if (timeout) {
+         clearTimeout(timeout);
+      }
       timeout = setTimeout(function () {
          updateTotalPrice();
-      }, 1000);
+      },200);
    });
-
    $("#addItem").on("submit", function (event) {
       event.preventDefault();
       var name = $(this).children("[name=name]").val();
       var price = $(this).children("[name=price]").val();
       var qty = $(this).children("[name=qty]").val();
-      var itemTotal = $(this).children("[name=itemTotal]").val();
-
-      $("tbody").append('<tr>' +' <td class="name">' + name + "</td>" +
-      '<td class="price">' + price + "</td>" + '<td class="qty">' + qty + "</td>" +
-      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' + '<td class="itemTotal">' + itemTotal + "</td>");
-
+      $("tbody").append('<tr>' + ' <td class="name">' + name + "</td>" +
+      '<td class="price">' + price + "</td>" + '<td class="qty input"><input min="0" type="number" value="' + qty + '"></td>' +
+      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' + '<td class="itemTotal"></td></tr>');
+      updateTotalPrice();
    });
 });
